7a15b41cce346ad6d3e9bac6fa3c8565610e4b3b
Cody Wisniewski <wisnieca@mail.uc.edu>
2015-02-03 12:07:52 -0500

    Finished feature8, removing stop word, and added test case

--- src/Tests/Test.java
+++ src/Tests/Test.java
@@ -2,6 +2,7 @@ package Tests;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -51,6 +52,35 @@ public class Test {
 		// removes last space, string aren't equal otherwise
 		outPutString = outPutString.substring(0, outPutString.length()-1);
 		assertEquals(outPutString, expectedOutPut);
+	}
+	
+	@org.junit.Test
+	public void testRemoveStopWords() {
+		String outPutString = "";
+		String[] stopWordArray = {"a","able","about","across","after","all",
+				"almost","also","am","among","an","and","any","are","as","at",
+				"be","because","been","but","by","can","cannot","could","dear",
+				"did","do","does","either","else","ever","every","for","from",
+				"get","got","had","has","have","he","her","hers","him","his",
+				"how","however","i","if","in","into","is","it","its","just",
+				"least","let","like","likely","may","me","might","most","must",
+				"my","neither","no","nor","not","of","off","often","on","only",
+				"or","other","our","own","rather","said","say","says","she","should",
+				"since","so","some","than","that","the","their","them","then","there",
+				"these","they","this","tis","to","too","twas","us","wants","was","we",
+				"were","what","when","where","which","while","who","whom","why","will",
+				"with","would","yet","you","your"};
+		Arrays.sort(stopWordArray);
+		String[] initialStringArray = {"An", "admin", "creates", "a", "LHCP", "an", "ER", "a", "LT", "or", "a", "PHA"};
+		String expectedOutput = "admin creates LHCP ER LT PHA";
+		for (int i=0; i<initialStringArray.length; i++) {
+			int index = Arrays.binarySearch(stopWordArray, initialStringArray[i].toLowerCase());
+	        if (index >= 0) {} else {
+	        	outPutString = outPutString + initialStringArray[i].toString() + " ";
+	        }
+		}
+		outPutString = outPutString.substring(0, outPutString.length()-1);
+		assertEquals(outPutString, expectedOutput);
 	}
 
 }
